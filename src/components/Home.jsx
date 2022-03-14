22bc95bb31bf4d31e7835e5aab29b0eab4a96ab3
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-03-14 20:00:49 +0530

    viewers component with on hover animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 const Home = () => {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   );
 };
