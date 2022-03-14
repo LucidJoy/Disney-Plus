9b4867fe748e01f1d65b36d9ffd64d1f812e9ac0
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-03-14 20:32:13 +0530

    fixed bug on Viewers component, added 4 more components and imported to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,20 @@ import styled from "styled-components";
 
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Trending from "./Trending";
 
 const Home = () => {
   return (
     <Container>
       <ImgSlider />
       <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   );
 };
--- src/components/Viewers.jsx
+++ src/components/Viewers.jsx
@@ -48,10 +48,11 @@ const Container = styled.div`
   padding: 30px 0 26px;
   display: grid;
   grid-gap: 25px;
+  gap: 25px;
   grid-template-columns: repeat(5, minmax(0, 1fr));
 
   @media (max-width: 768px) {
-    grid-template-columns: repeat(5, minmax(0, 1fr));
+    grid-template-columns: repeat(1, minmax(0, 1fr));
   }
 `;
 
